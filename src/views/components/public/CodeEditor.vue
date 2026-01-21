4b2b8287547abdd48e7c54f2a3798b1eb780df74
lxp <2512142371@qq.com>
2026-01-21 16:31:19 +0800

    去掉codemirror依赖

--- src/views/components/public/CodeEditor.vue
+++ src/views/components/public/CodeEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="code-editor">
     <!-- 使用导入的 Codemirror 组件 -->
-    <Codemirror
+    <!-- <Codemirror
       v-model:value="props.value"
       :autofocus="true"
       :placeholder="placeholder"
@@ -10,7 +10,7 @@
       :lineWrapping="true"
       :style="{ height: '300px', border: '1px solid #ddd', borderRadius: '4px' }"
       :extensions="extensions"
-    />
+    /> -->
   </div>
 </template>
 
@@ -19,8 +19,8 @@ import { ref, computed, watch } from 'vue';
 import { javascript } from '@codemirror/lang-javascript';
 import { html } from '@codemirror/lang-html';
 import { oneDark } from '@codemirror/theme-one-dark';
-import { EditorView } from '@codemirror/view'; // 用于自定义视图配置
-import { Codemirror } from 'vue-codemirror'; // 引入组件
+// import { EditorView } from '@codemirror/view'; // 用于自定义视图配置
+// import { Codemirror } from 'vue-codemirror'; // 引入组件
 
 const props = defineProps({
   value: { type: String, default: '' },
